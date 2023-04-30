cad8e8f75dd45f7aacd43e3a5e79b8ca152b13cb
Chris Green <ccgreen93@gmail.com>
2023-04-30 23:33:52 +1000

    comment

--- scripts/copy_dirs.sh
+++ scripts/copy_dirs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# run script after entering new `screen` session when copying large amounts of data
+
 cat savelist.txt | while read i; do
 echo "copying: '$i'"
 cp -R "/mnt/v1_rz2/movies/$i" /mnt/wd_passport/movies
