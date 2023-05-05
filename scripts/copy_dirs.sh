73b84130e9432fd32baf56393973f743f528be12
Chris Green <ccgreen93@gmail.com>
2023-05-05 23:56:51 +1000

    refactor

--- scripts/copy_dirs.sh
+++ scripts/copy_dirs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # run script after entering new `screen` session when copying large amounts of data
+textfile="savelist.txt"
+source="/mnt/v1_rz2/series"
+dest="/mnt/wd_passport/series"
 
-cat savelist.txt | while read i; do
-echo "copying: '$i'"
-cp -R "/mnt/v1_rz2/movies/$i" /mnt/wd_passport/movies
+cat "$textfile" | while read i; do
+  echo "copying: '$i'"
+  cp -R "${source}/$i" "${dest}"
 done
