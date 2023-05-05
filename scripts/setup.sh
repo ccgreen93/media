3299b57a4c42596c1e423caba8f5e1de89001e5e
cg-mediacenter <ccgreen93@gmail.com>
2023-05-05 13:59:10 +0000

    chmod +x

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,19 +5,19 @@ script_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 parent_dir="$(dirname "$script_path")"
 
 # Make users and group
-sudo useradd sonarr -u 3001
-sudo useradd radarr -u 3002
-sudo useradd prowlarr -u 3003
-sudo useradd jackett -u 3004
-sudo useradd qbittorrent -u 3005
-sudo useradd overseerr -u 3006
-sudo groupadd mediacenter -g 3000
-sudo usermod -a -G mediacenter sonarr
-sudo usermod -a -G mediacenter radarr
-sudo usermod -a -G mediacenter prowlarr
-sudo usermod -a -G mediacenter qbittorrent
-sudo usermod -a -G mediacenter jackett
-sudo usermod -a -G mediacenter overseerr
+#sudo useradd sonarr -u 3001
+#sudo useradd radarr -u 3002
+#sudo useradd prowlarr -u 3003
+#sudo useradd jackett -u 3004
+#sudo useradd qbittorrent -u 3005
+#sudo useradd overseerr -u 3006
+#sudo groupadd mediacenter -g 3000
+#sudo usermod -a -G mediacenter sonarr
+#sudo usermod -a -G mediacenter radarr
+#sudo usermod -a -G mediacenter prowlarr
+#sudo usermod -a -G mediacenter qbittorrent
+#sudo usermod -a -G mediacenter jackett
+#sudo usermod -a -G mediacenter overseerr
 
 # Make directories
 sudo mkdir -pv "${parent_dir}/config/{sonarr,radarr,prowlarr,jackett,qbittorrent,overseerr}"
@@ -42,4 +42,4 @@ nfs_mount_base_path="${parent_dir}/data/media"
 echo "${nfs_server}:${nfs_share_base_path}/movies ${nfs_mount_base_path}/movies nfs rw,noatime,rsize=131072,wsize=131072,hard,intr,timeo=150,retrans=3 0 0" >> /etc/fstab
 echo "${nfs_server}:${nfs_share_base_path}/series ${nfs_mount_base_path}/series nfs rw,noatime,rsize=131072,wsize=131072,hard,intr,timeo=150,retrans=3 0 0" >> /etc/fstab
 
-# echo "UID=$(id -u)" >> "${parent_dir}/.env"
+## echo "UID=$(id -u)" >> "${parent_dir}/.env"
