ccf12bbb4ed77f433f3a8e51775be16119ae43ac
cg-mediacenter <ccgreen93@gmail.com>
2023-05-13 12:11:45 +0000

    enhancement

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,6 +11,7 @@ nfs_mount_base_path="${parent_dir}/data"
 
 create_dirs() {
   # Make directories
+  printf "making directories..."
   sudo mkdir -pv "${parent_dir}"/config/{sonarr,radarr,prowlarr,qbittorrent,overseerr}
   sudo mkdir -pv "${parent_dir}"/data/{torrents,media}/{series,movies}
 }
@@ -18,25 +19,27 @@ create_dirs() {
 nfs() {
 
   # check nfs server provider
-  [ -z "$1" ] && echo "Skipping NFS setup, no server provided" && return
+  [ -z "$1" ] && printf "Skipping NFS setup, no server provided" && return
 
   nfs_server=$1
   nfs_share_base_path=$2
   nfs_mount_base_path=$3
 
   if ! command -v which tee &> /dev/null; then
-    echo "tee not found, installing"
+    printf "tee not found, installing"
     sudo apt install tee -y
   fi
 
   # Add nfs shares to fstab
   # configured for 128KB record sizes
+  printf "updating fstab..."
   printf "%s %s %s %s 0 0" "${nfs_server}:${nfs_share_base_path}/movies" "${nfs_mount_base_path}/media/movies" "nfs" "rw,noatime,rsize=131072,wsize=131072,hard,intr,timeo=150,retrans=3" | sudo tee -a /etc/fstab
   printf "%s %s %s %s 0 0" "${nfs_server}:${nfs_share_base_path}/series" "${nfs_mount_base_path}/media/series" "nfs" "rw,noatime,rsize=131072,wsize=131072,hard,intr,timeo=150,retrans=3" | sudo tee -a /etc/fstab
   printf "%s %s %s %s 0 0" "${nfs_server}:${nfs_share_base_path}/torrents" "${nfs_mount_base_path}/torrents" "nfs" "rw,noatime,rsize=131072,wsize=131072,hard,intr,timeo=150,retrans=3" | sudo tee -a /etc/fstab
 
   # add nfs service dependency for docker to ensure
   # nfs shares are mounted before containers start
+  printf "adding service for ensuring shares are mounted before docker starts..."
   sudo mkdir /etc/systemd/system/docker.service.d/
   printf "%s\n" \
     "[Unit]" \
@@ -48,16 +51,19 @@ nfs() {
 
 set_permissions() {
   # Set permissions
+  printf "setting folder permissions..."
   sudo chmod -R 775 "${parent_dir}"/{data,media}
   sudo chown -R "$(id -u)":"$(id -g)" "${parent_dir}"/{data,media}
 }
 
 update_uid_env() {
   # add vars to .env file
-  printf "IP=%s" "$(hostname -I | awk '{print $1}')" | tee -a "${parent_dir}"/.env
-  printf "BASE_DIR=%s" "${1}" | tee -a "${parent_dir}"/.env
-  printf "UID=%s" "$(id -u)" | tee -a "${parent_dir}"/.env
-  printf "GID=%s" "$(id -g)" | tee -a "${parent_dir}"/.env
+  parent_dir=$1
+  printf "updating .env file..."
+  sed -i "/IP=/c\IP=\"$(hostname -I | awk '{print $1}')\"" "${parent_dir}"/.env
+  sed -i "/BASE_DIR=/c\BASE_DIR=\"${parent_dir}\"" "${parent_dir}"/.env
+  sed -i "/UID=/c\UID=$(id -u)" "${parent_dir}"/.env
+  sed -i "/GID=/c\GID=$(id -g)" "${parent_dir}"/.env
 }
 
 
